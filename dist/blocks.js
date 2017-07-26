68cc0f0553e592a4de7a720a4334026dd1ec2d6f
godspeedelbow <eelke@boezeman.com>
2017-07-26 09:50:35 +0200

    added blocks.js to dist

--- dist/blocks.js
+++ dist/blocks.js
@@ -3,7 +3,7 @@
 var _require = require('./2dArrayUtils'),
     mapCells = _require.mapCells;
 
-module.exports = [[[1, 1], [1, 1]], [[1], [1], [1], [1]], [[1, 1, 0], [0, 1, 1]], [[0, 1, 1], [1, 1, 0]], [[1, 1, 0], [1, 0, 0], [1, 0, 0]], [[1, 0, 0], [1, 0, 0], [1, 1, 0]]].map(colorBlock);
+module.exports = [[[1, 1], [1, 1]], [[1], [1], [1], [1]], [[1, 1, 0], [0, 1, 1]], [[0, 1, 1], [1, 1, 0]], [[1, 1], [1, 0], [1, 0]], [[1, 0], [1, 0], [1, 1]], [[1, 0], [1, 1], [1, 0]]].map(colorBlock);
 
 function colorBlock(block, index) {
     return mapCells(block, function (cell) {
